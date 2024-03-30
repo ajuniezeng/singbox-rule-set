5f86d2f7cec17b6018ac88048ac5d00ce3a20987
Ajunie Zeng <ajuniezeng@gmail.com>
2024-03-30 09:51:01 +0100

    fix

--- .github/release-rule-set.sh
+++ .github/release-rule-set.sh
@@ -4,8 +4,7 @@ set -e -o pipefail
 
 git config --local user.email "github-action@users.noreply.github.com"
 git config --local user.name "GitHub Action"
-git remote add origin https://github-action:$GITHUB_TOKEN@github.com/ajuniezeng/singbox-rule-set.git
-git pull origin master 
+git remote add update https://github-action:$GITHUB_TOKEN@github.com/ajuniezeng/singbox-rule-set.git 
 git add .
 git commit -m "daily update"
-git push -f origin master
+git push -f update master
