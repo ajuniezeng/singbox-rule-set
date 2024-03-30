37d83f6c79ccc02fd8ac5c1292be5d4d3fc22e5f
Ajunie Zeng <ajuniezeng@gmail.com>
2024-03-30 08:48:41 +0100

    add GitHub Action

---
+++ .github/build.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+# set up the environment
+sudo apt-get update
+
+VERSION=$(curl -s https://api.github.com/repos/SagerNet/sing-box/releases/latest \
+    | grep tag_name \
+    | cut -d ":" -f2 \
+    | sed 's/\"//g;s/\,//g;s/\ //g;s/v//')
+
+curl -Lo sing-box.tar.gz "https://github.com/SagerNet/sing-box/releases/download/v${VERSION}/sing-box-${VERSION}-linux-amd64.tar.gz"
+curl -Lo geoip.db "https://github.com/lyc8503/sing-box-rules/releases/latest/download/geoip.db"
+curl -Lo geosite.db "https://github.com/lyc8503/sing-box-rules/releases/latest/download/geosite.db"
+
+tar -xzvf sing-box.tar.gz
+mv ./sing-box-${VERSION}-linux-amd64/sing-box .
+chmod +x sing-box
+
+geoipAddresses=("cn" "de" "facebook" "google" "netflix" "telegram" "twitter")
+geositeDomains=("amazon" "apple" "bilibili" "category-ads-all" "category-games" "cn" "discord" "disney" "facebook" "geolocation-!cn" "github" "google" "instagram" "microsoft" "netflix" "onedrive" "openai" "primevideo" "telegram" "tiktok" "tld-!cn" "twitch" "hbo" "twitter" "youtube" "threads" "nvidia" "category-httpdns")
+
+for item in "${geoipAddresses[@]}"; do
+    ./sing-box geoip export "$item"
+done
+
+for item in "${geositeDomains[@]}"; do
+    ./sing-box geosite export "$item"
+done
+
+mkdir -p bin rule-set
+for item in *.json; do
+    ./sing-box rule-set compile "$item"
+    mv "${item%.json}.srs" bin/
+    mv "$item" rule-set/
+done
+
+rm -rf sing-box.tar.gz sing-box-${VERSION}-linux-amd64/ geoip.db geosite.db sing-box
+
+git config --local user.email "github-action@users.noreply.github.com"
+git config --local user.name "GitHub Action"
+git remote add origin https://github-action:$GITHUB_TOKEN@github.com/ajuniezeng/singbox-rule-set.git
+git pull origin master 
+git add .
+git commit -m "daily update"
+git push -f origin master
