ed83fb49bd118b1289591c90d15cad816a9887a4
Ajunie Zeng <ajuniezeng@gmail.com>
2024-03-30 09:32:03 +0100

    fix

--- .github/build.sh
+++ .github/build.sh
@@ -2,6 +2,19 @@
 
 set -e -o pipefail
 
+VERSION=$(curl -s https://api.github.com/repos/SagerNet/sing-box/releases/latest \
+| grep tag_name \
+| cut -d ":" -f2 \
+| sed 's/\"//g;s/\,//g;s/\ //g;s/v//')
+
+curl -Lo sing-box.tar.gz "https://github.com/SagerNet/sing-box/releases/download/v${VERSION}/sing-box-${VERSION}-linux-amd64.tar.gz"
+curl -Lo geoip.db "https://github.com/lyc8503/sing-box-rules/releases/latest/download/geoip.db"
+curl -Lo geosite.db "https://github.com/lyc8503/sing-box-rules/releases/latest/download/geosite.db"
+
+tar -xzvf sing-box.tar.gz
+mv ./sing-box-${VERSION}-linux-amd64/sing-box .
+chmod +x sing-box
+
 geoipAddresses=("cn" "de" "facebook" "google" "netflix" "telegram" "twitter")
 geositeDomains=("amazon" "apple" "bilibili" "category-ads-all" "category-games" "cn" "discord" "disney" "facebook" "geolocation-!cn" "github" "google" "instagram" "microsoft" "netflix" "onedrive" "openai" "primevideo" "telegram" "tiktok" "tld-!cn" "twitch" "hbo" "twitter" "youtube" "threads" "nvidia" "category-httpdns")
 
@@ -19,3 +32,5 @@ for item in *.json; do
     mv "${item%.json}.srs" bin/
     mv "$item" rule-set/
 done
+
+rm -rf sing-box.tar.gz sing-box-${VERSION}-linux-amd64/ geoip.db geosite.db sing-box
--- .github/release-rule-set.sh
+++ .github/release-rule-set.sh
@@ -2,8 +2,6 @@
 
 set -e -o pipefail
 
-rm -rf sing-box.tar.gz sing-box-${VERSION}-linux-amd64/ geoip.db geosite.db sing-box
-
 git config --local user.email "github-action@users.noreply.github.com"
 git config --local user.name "GitHub Action"
 git remote add origin https://github-action:$GITHUB_TOKEN@github.com/ajuniezeng/singbox-rule-set.git
